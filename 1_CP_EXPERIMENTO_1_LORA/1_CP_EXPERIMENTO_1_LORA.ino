2ad49c372c11db6b262ef312f21807553632b8d5
blanca <blancamendez2803@gmail.com>
2024-01-19 16:59:26 +0100

    Codigos experimentos 1 y 2 del nodo LoRa
    Se adjunta el codigo ejecutado para el experimento 1 (seccion 3.3.1 del TFG) del nodo LoRa: 1_CP_EXPERIMENTO_1_LORA
    Se adjunta el codigo ejecutado para el experimento 2 (seccion 3.3.2 del TFG) del nodo LoRa: 2_CP_EXPERIMENTO_2_LORA

---
+++ 1_CP_EXPERIMENTO_1_LORA/1_CP_EXPERIMENTO_1_LORA.ino
@@ -0,0 +1,67 @@
+// Autora: Blanca Mendez Lopez
+// Codigo empleado en la seccion 3.3.1 del TFG para realizar el primer experimento del nodo LoRa
+
+// Importacion de librerias
+#include <MKRWAN.h>
+#include "Adafruit_SHTC3.h"
+#include "ArduinoLowPower.h"
+
+// Instancia de objetos 
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3(); // Permite la interaccion con el sensor SHTC3
+LoRaModem LoRa; // Permite la configuracion y el control de la comunicacion LoRa
+
+// Configuracion de los parametros de LoRa en The Things Network (TTN)
+String appEui = "0000000000000000"; // EUI de la aplicacion en TTN
+String appKey = "4A25DC5E841C169C262934B9FF12DE4C"; // Key de la aplicacion en TTN
+
+// Constante que representa el intervalo de tiempo antes de realizar nuevo envio de datos, en milisegundos
+const unsigned long interval = 900000; // 15 minutos
+
+// Variables para almacenar las medidas de humedad y temperatura captadas por el sensor
+sensors_event_t humidity, temp; 
+
+
+// Configuracion inicial
+void setup() {
+  
+  // Configuracion e inicializacion del modulo LoRa en la region EU868 (UE)
+  if (!LoRa.begin(EU868)) { 
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+
+  // Union a TTN utilizando OTAA
+  if (!LoRa.joinOTAA(appEui, appKey)) { 
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+  
+  LoRa.setPort(3); // Se configura el puerto LoRa en 3
+
+  // Inicializacion del sensor SHTC3
+  if (!shtc3.begin()) { 
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+}
+
+// Bucle principal
+void loop() {
+  SenseAndSend();
+  GoToSleep();
+}
+
+// Funcion que mide la temperatura y humedad y envia el mensaje a traves de LoRa
+void SenseAndSend() {
+  shtc3.getEvent(&humidity, &temp); // Se realizan las medidas de temperatura y humedad
+  // Se construye una cadena de mensaje que contiene las medidas
+  String message = "Temperatura: " + String(temp.temperature) + ", Humedad relativa: " + String(humidity.relative_humidity);
+  // Proceso de envio del mensaje
+  LoRa.beginPacket(); // Inicializacion de un paquete LoRa
+  LoRa.print(message); // Se imprime la cadena de mensaje en el paquete creado
+  LoRa.endPacket(); // Se finaliza el paquete
+  LoRa.sleep(); // Se pone el modulo LoRa en modo de bajo consumo
+}
+
+// Funcion que pone al Arduino MKR WAN 1300 en modo de bajo consumo (suspension profunda)
+void GoToSleep(){
+  LowPower.deepSleep(interval); // Esta en este modo durante el tiempo interval
+}
+
---
+++ 2_CP_EXPERIMENTO_2_LORA/2_CP_EXPERIMENTO_2_LORA.ino
@@ -0,0 +1,73 @@
+// Autora: Blanca Mendez Lopez
+// Codigo empleado en la seccion 3.3.2 del TFG para realizar el segundo experimento del nodo LoRa
+
+// Importacion de librerias
+#include <MKRWAN.h>
+#include "Adafruit_SHTC3.h"
+
+// Instancia de objetos
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3(); // Permite la interaccion con el sensor SHTC3
+LoRaModem LoRa; // Permite la configuracion y el control de la comunicacion LoRa
+
+// Configuracion de los parametros de LoRa en The Things Network (TTN)
+String appEui = "0000000000000000"; // EUI de la aplicacion en TTN
+String appKey = "4A25DC5E841C169C262934B9FF12DE4C"; // Key de la aplicacion en TTN
+
+// Variables para almacenar las medidas de humedad y temperatura captadas por el sensor
+sensors_event_t humidity, temp;
+
+// Constate para el pin Done del temporizador de alimentacion que al activarse le indica que la tarea se ha realizado con exito y ya puede cortar la alimentacion del nodo
+const byte pinDone = 0;
+
+
+// Configuracion inicial
+void setup() {
+
+  pinMode(pinDone, OUTPUT); // Se configura el pin como una salida digital
+  pinMode(pinDone, LOW); // Se establece en un estado desactivado
+
+  // Configuracion e inicializacion del modulo LoRa en la region EU868 (UE)
+  if (!LoRa.begin(EU868)) {
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+
+  // Union a TTN utilizando OTAA
+  if (!LoRa.joinOTAA(appEui, appKey)) {
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+
+  LoRa.setPort(3); // Se configura el puerto LoRa en 3
+
+  // Inicializacion del sensor SHTC3
+  if (!shtc3.begin()) {
+    while (1); // Si la inicializacion no fue exitosa entra en un bucle infinito y se detiene la ejecucion del programa
+  }
+}
+
+// Bucle principal
+void loop() {
+  SenseAndSend();
+  GoToSleep();
+}
+
+// Funcion que mide la temperatura y humedad y envia el mensaje a traves de LoRa
+void SenseAndSend() {
+  shtc3.getEvent(&humidity, &temp); // Se realizan las medidas de temperatura y humedad
+  // Se construye una cadena de mensaje que contiene las medidas
+  String message = "Temperatura: " + String(temp.temperature) + ", Humedad relativa: " + String(humidity.relative_humidity);
+  // Proceso de envio del mensaje
+  LoRa.beginPacket(); // Inicializacion de un paquete LoRa
+  LoRa.print(message); // Se imprime la cadena de mensaje en el paquete creado
+  LoRa.endPacket(); // Se finaliza el paquete
+  delay(8500); // Espera de 8.5s antes de cortar la alimentacion del nodo para que a TTN le de tiempo a recibir y procesar el paquete
+}
+
+// Funcion que controla el pin Done del temporizador de alimentacion para que corte la alimentacion y la vuelva a activar
+void GoToSleep() {
+  
+  digitalWrite(pinDone, HIGH); // Se activa el pin Done por lo que se corta la alimentacion durante el tiempo establecido
+                              // por la resistencia conectada al pin Delay del temporizador de alimentacion
+  delay(1);
+  digitalWrite(pinDone, LOW); // Pasado el tiempo establecido, se desactiva el pin Done y activa la alimentacion del nodo
+  delay(1);
+}
